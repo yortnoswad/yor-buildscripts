3df98c06b3a0922c9001fab360e8f538a0443f7a
Limpha Yoklme <limpha@gmail.com>
2015-06-13 10:10:46 -0500

    Make buildscripts more customizable

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # yorlinux-buildscripts
 Scripts, libraries, and configuration files used to build Yor Linux
+
+To setup buildscripts on your machine
+ * Download this git repo
+ * Change to this directory
+ * Edit usr/local/etc/buildscripts.conf to match your enviroment
+ * Run setup.sh
--- usr/local/bin/update.repolist.sh
+++ usr/local/bin/update.repolist.sh
@@ -1,26 +1,58 @@
+#!/bin/bash
+#
+# This script 
+
+# Get the buildscripts global variables
+source /usr/local/bin/buildscripts.conf
+
+# Set the variables specific to this script
+REPODIR="$WORKDIR/repolist"
+NOW=$(date +"%Y-%m-%d %H:%M")
 TODAY=$(date +%Y-%m-%d)
-REPODIR="/srv/gitrepo/lists/repolist"
-BINDIR="/srv/gitrepo/centos/centos-git-common"
 TODAYLIST="$REPODIR/repolist-$TODAY"
 YESTERDAYLIST="$REPODIR/repolist-YESTERDAY"
 MAILFILE="$REPODIR/mailfile.$TODAY"
-LOGFILE="$REPODIR/logfile"
+HISTORYFILE="$REPODIR/history"
+NEWREPOFILE="$REPODIR/newrepo"
 
-date >> $LOGFILE
+# Pull down the full centos repo list, and massage it into a nice file
+#$BINDIR/centos.git.repolist.py -b c7 | cut -d'/' -f6 | grep .git | rev | cut -c 5- | rev | sort -u > $TODAYLIST
+$BINDIR/centos.git.repolist.py -b c7 | cut -d'/' -f6 | grep .git | while read line
+do
+  URL="https://git.centos.org/git/rpms/$line"
+  REPO="$(echo $line | rev | cut -c 5- | rev )"
+  echo "$REPO,$URL" >> $TODAYLIST
+done
+sort -u -o $TODAYLIST $TODAYLIST
 
-#1 - Get repo list
-#./centos.git.repolist.py -b c7 > /tmp/repolist
-#cat /tmp/repolist | cut -d'/' -f6 | grep .git | rev | cut -c 5- | rev | sort -u > /tmp/repolist.trimmed
-$BINDIR/centos.git.repolist.py -b c7 | cut -d'/' -f6 | grep .git | rev | cut -c 5- | rev | sort -u > $TODAYLIST
+# Make sure we have yesterdays list, even if it is empty
+[ -f $YESTERDAYLIST ] || touch $YESTERDAYLIST
+
+# Compare todays repolist with yesterdays, and act accordingly
 if $(diff --brief $TODAYLIST $YESTERDAYLIST > /dev/null) ; then
   # There was no package added
+  # If there was no change, get rid of the clutter
   /bin/rm -f $TODAYLIST
+  # Be sure to log that we ran and our status
+  echo "$NOW [SUCCESS] $0 [NO CHANGE]" >> $LOGFILE
 else
   # There was a package added
-  comm -23 $TODAYLIST $YESTERDAYLIST >> $MAILFILE
-  mail -s "NEW PACKAGES - $TODAY" yortnoswad@gmail.com < $MAILFILE
-  cat $MAILFILE >> $LOGFILE
+  # List the changes and put them in the mail file
+  comm -23 $TODAYLIST $YESTERDAYLIST > $MAILFILE
+  # Send our email out
+  mail -s "NEW REPOS - $TODAY" $EMAILLIST < $MAILFILE
+  # Add this list to the history file, with date stamp
+  echo $TODAY >> $HISTORYFILE
+  cat $MAILFILE >> $HISTORYFILE
+  # Add this list to the newrepo file
+  #   The new repo file should be cleaned out when it is 
+  #   determined what categories the repos fall in
+  cat $MAILFILE >> $NEWREPOFILE
+  # Change the link so we are ready for the next run
   ln -sf $TODAYLIST $YESTERDAYLIST
+  # cleanup
   /bin/rm -f $MAILFILE
+  # Be sure to log that we ran and our status
+  echo "$NOW [SUCCESS] $0 [NEW REPOS] $NEWREPOFILE" >> $LOGFILE
 fi
 
