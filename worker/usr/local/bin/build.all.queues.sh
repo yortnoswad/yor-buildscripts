58fe985d2e61e8fc55b52bf216ef5147049bdb8b
Limpha Yoklme <limpha@gmail.com>
2015-10-21 20:29:23 -0500

    initial import

---
+++ worker/usr/local/bin/build.all.queues.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# Check and see if there are any new repos (packages)
+#   in the list of centos rpm git repo's
+#
+# If there are, send out an email, and put the new
+#   repos in the newrepo file
+#
+
+# Get the buildscripts global variables
+source /usr/local/etc/buildscripts.conf
+
+# Setup Variables
+DEBUG="true"
+MONTH="$(date +%Y-%m)"
+WORKDIR="$BUILDDIR/working/$BUILDHOST"
+LOCKFILE="$WORKDIR/lockfile"
+LOGFILE="$WORKDIR/logs/$MONTH.log"
+VERSIONLIST="yor7 el7 7_1 7_2"
+HOSTARCH="$(uname -i)"
+case $HOSTARCH in
+  armv7 | armv7l | armv7hl )
+    ARCHLIST="armv7"
+    ;;
+  i386 )
+    ARCHLIST="i386"
+    ;;
+  x86_64 )
+    ARCHLIST="x86_64 i386"
+    ;;
+  * )
+    echo "No build queues for arch: $HOSTARCH"
+    exit 1
+    ;;
+esac
+if [ "$HOSTARCH" == "$NOARCHBUILDREALARCH" ] ; then
+  ARCHLIST="$ARCHLIST noarch"
+fi
+
+# Only run if we aren't already running
+if [ -f $LOCKFILE ] ; then
+  exit 5
+else
+  # Create lockfile
+  echo $$ > $LOCKFILE
+  
+  # Setup dirs
+  mkdir -p $WORKDIR $WORKDIR/logs
+  
+  # Start logs
+  echo "===============" >> $LOGFILE
+  date +%Y-%m-%d::%H:%M >> $LOGFILE
+  echo "-----" >> $LOGFILE
+  
+  # Run through all the versions, for each arch we are doing
+  for THISARCH in $ARCHLIST
+  do
+    for THISVERSION in $VERSIONLIST
+    do
+      if [ "$DEBUG" == "true" ] ; then
+        echo "  Running: $THISARCH $THISVERSION" >> $LOGFILE
+      fi
+      rebuild.packages.sh $THISARCH $THISVERSION
+    done
+  done
+  # Remove lockfile
+  /bin/rm -f $LOCKFILE
+
+fi
