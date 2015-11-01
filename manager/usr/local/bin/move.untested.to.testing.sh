75cc71759ac129a788ea7e8482f0ebd7d03e92df
Limpha Yoklme <limpha@gmail.com>
2015-10-31 19:22:01 -0500

    use extras

--- manager/usr/local/bin/move.untested.to.testing.sh
+++ manager/usr/local/bin/move.untested.to.testing.sh
@@ -186,15 +186,15 @@ if [ -s $MAILFILE ] ; then
   fi
   # Clear out the untested files that are now in tested
   cd $YORREPODIRs
-  ls -1 7testing/i386/updates/security/ 7testing/i386/updates/fastbugs/ 7testing/i386/os/Packages/  | while read line
+  ls -1 7testing/i386/updates/security/ 7testing/i386/updates/fastbugs/ 7testing/i386/os/Packages/ 7/extras/i386/ | while read line
     do 
       rm -f 7untested/i386/os/Packages/$line
     done
-  ls -1 7testing/x86_64/updates/security/ 7testing/x86_64/updates/fastbugs/ 7testing/x86_64/os/Packages/  | while read line
+  ls -1 7testing/x86_64/updates/security/ 7testing/x86_64/updates/fastbugs/ 7testing/x86_64/os/Packages/ 7/extras/x86_64/ | while read line
     do 
       rm -f 7untested/x86_64/os/Packages/$line
     done
-  ls -1 7testing/armv7/updates/security/ 7testing/armv7/updates/fastbugs/ 7testing/armv7/os/Packages/  | while read line
+  ls -1 7testing/armv7/updates/security/ 7testing/armv7/updates/fastbugs/ 7testing/armv7/os/Packages/ 7/extras/armv7/ | while read line
     do 
       rm -f 7untested/armv7/os/Packages/$line
     done
